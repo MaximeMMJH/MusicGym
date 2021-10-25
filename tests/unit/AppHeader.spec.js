2b027019e47bcf871276d61c4c16d5a7249f14f3
Heuver,Maxime M.M.J <maxime.heuver@gmail.com>
2021-10-25 12:47:20 +0200

    added jest and example test

---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,15 @@
+import AppHeader from "@/components/AppHeader.vue";
+import { mount } from "@vue/test-utils";
+
+describe("AppHeader", () => {
+  it("hide button when user is logged off", async () => {
+    const wrapper = mount(AppHeader);
+    expect(wrapper.find("button").isVisible()).toBe(false);
+  });
+
+  it("show button when user is logged in", async () => {
+    const wrapper = mount(AppHeader);
+    await wrapper.setData({ loggedIn: true });
+    expect(wrapper.find("button").isVisible()).toBe(true);
+  });
+});
