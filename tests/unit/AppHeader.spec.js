3751f914848a394b9c82ec8051ed3009fadb6994
Heuver,Maxime M.M.J <maxime.heuver@gmail.com>
2022-01-13 13:00:14 +0100

    removed failing test

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -12,10 +12,4 @@ describe("AppHeader", () => {
     await wrapper.setData({ loggedIn: true });
     expect(wrapper.find("button").isVisible()).toBe(true);
   });
-
-  it("show button when user is logged in", async () => {
-    const wrapper = mount(AppHeader);
-    await wrapper.setData({ loggedIn: true });
-    expect(wrapper.find("button").isVisible()).toBe(false);
-  });
 });
